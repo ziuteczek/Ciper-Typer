15c699bcc344f4fcbc1e936d3f2af07c678b3814
Stanisław Dymny <dymnystan@gmail.com>
2023-12-09 13:56:56 +0100

    game text creator added

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-'use strict'
-function generateText(length,...letters){
-    letters.push(" ");
-    
+"use strict";
+const textEl = document.querySelector(".text");
+
+
+const randIndex = (arrLength) => Math.floor(Math.random() * arrLength);
+function generateText(length, ...letters) {
+  let text = [];
+  letters.push(" ");
+  for (let i = 0; i < length; i++) {
+    if (i === 0 || i === length - 1 || text[i - 1] === " ") {
+      text.push(letters[randIndex(letters.length - 1)]);
+    } else {
+      text.push(letters[randIndex(letters.length)]);
+    }
+  }
+  return text;
+}
+function startGame(gameContainer) {
+  const text = generateText(20, "k", "l", "m").map((e) => {
+    const el = document.createElement("span");
+    el.textContent = e;
+    gameContainer.append(el);
+    return el;
+  });
+  letterToGuess = 0;
+  
 }
 // #f4f0f0
