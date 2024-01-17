cd60a05aaa21c7b5b6dab353105e1daa46a4b37d
Stanisław Dymny <dymnystan@gmail.com>
2024-01-17 02:49:22 +0100

    menu created

--- script.js
+++ script.js
@@ -2,28 +2,14 @@
 const textEl = document.querySelector('.text')
 const timeEl = document.querySelector('.time')
 
-const isLetter = sign => /^[a-zA-Z\s]$/.test(sign)
-
-const randIndex = arrLength => Math.floor(Math.random() * arrLength)
-function generateText(length, ...letters) {
-  let text = []
-  letters.push('_')
-  for (let i = 0; i < length; i++) {
-    if (i === 0 || i === length - 1 || text[i - 1] === ' ') {
-      text.push(letters[randIndex(letters.length - 1)])
-    } else {
-      text.push(letters[randIndex(letters.length)])
-    }
-  }
-  return text
-}
 async function startGame(gameContainer) {
   const quizLength = 20
   const letters = ['k', 'l', 'm']
   let mistakeCount = 0
   let letterToGuess = 0
   let time = 0
-  const text = generateText(quizLength, ...letters).map(e => {
+  const textArr = generateText(quizLength, ...letters)
+  const text = textArr.map(e => {
     const el = document.createElement('span')
     el.textContent = e
     gameContainer.append(el)
@@ -47,8 +33,14 @@ async function startGame(gameContainer) {
     }
   }
   clearInterval(timeInterval)
-  console.log(mistakeCount)
-  console.log(Number(timeEl.textContent))
+  const totalTime = Number(timeEl.textContent)
+  const words =
+    textArr.reduce(
+      (spaceCount, sign) => (sign === '_' ? spaceCount + 1 : spaceCount),
+      0
+    ) + 1
+  const wpm = Math.round((words / totalTime) * 100)
+  // endGame(totalTime, words, wpm)
 }
 const getKey = () => {
   return new Promise(resolve => {
@@ -57,9 +49,20 @@ const getKey = () => {
     })
   })
 }
-const resultModal = (time, text) => {
-  const modal = document.createElement('div')
-  const blur = document.createElement('div')
+const isLetter = sign => /^[a-zA-Z\s]$/.test(sign)
+
+const randIndex = arrLength => Math.floor(Math.random() * arrLength)
+function generateText(length, ...letters) {
+  let text = []
+  letters.push('_')
+  for (let i = 0; i < length; i++) {
+    if (i === 0 || i === length - 1 || text[i - 1] === '_') {
+      text.push(letters[randIndex(letters.length - 1)])
+    } else {
+      text.push(letters[randIndex(letters.length)])
+    }
+  }
+  return text
 }
 // #f4f0f0
 startGame(textEl)
